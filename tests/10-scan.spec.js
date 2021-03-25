5998b0a94d967665b891e99b73352a1bb6e68768
Matthew Collier <mcollier@digitalbazaar.com>
2021-03-25 11:13:24 -0400

    Fix tests.

--- tests/10-scan.spec.js
+++ tests/10-scan.spec.js
@@ -1,7 +1,7 @@
 /*!
  * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
  */
-import {scan} from '../lib/scan.js';
+import {scan} from '..';
 
 describe('Scan Tests', () => {
   it(`should properly export authorize`, async () => {
